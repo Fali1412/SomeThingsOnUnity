2088547e8b983948f6c6cfdb1c753a3c8175a94e
Fali1412 <fattev.ilya1337@gmail.com>
2020-12-06 16:25:54 +0700

    First commit math graph

---
+++ Assets/Graph.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab;
+
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+
+    Transform[] points;
+
+    void Awake() {
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
+
+        points = new Transform[resolution];
+        for(int x = 0; x < points.Length; x++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (x + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+            points[x] = point;
+        }
+    }
+
+    void Update() {
+        float time = Time.time;
+
+        for(int x = 0; x < points.Length; x++)
+        {
+            Transform point = points[x];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
+    }
+}
